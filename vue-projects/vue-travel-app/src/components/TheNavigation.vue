70bfff5eba57e76e5c416a01a74e3f47837b837b
GameDev <carlos.motta.business@gmail.com>
2023-06-29 23:50:40 +0900

    Change folder structure. Add vue travel app project.

---
+++ vue-projects/vue-travel-app/src/components/TheNavigation.vue
@@ -0,0 +1,20 @@
+<script>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav id="nav">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/brazil">Brazil</RouterLink>
+        <RouterLink to="/jamaica">Jamaica</RouterLink>
+        <RouterLink to="/panama">Panama</RouterLink>
+    </nav>
+</template>
+
+<!-- <style lang="css">
+#nav .custom-active {
+    color: red;
+    border-bottom: 1px solid red;
+}
+</style> -->
---
+++ vue-projects/vue-travel-app/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {path: '/brazil', name: 'brazil',component: () => import('../views/Brazil.vue')},
+  {path: '/jamaica', name: 'jamaica',component: () => import('../views/Jamaica.vue')},
+  {path: '/panama', name: 'panama',component: () => import('../views/Panama.vue')},
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  // linkActiveClass: 'custom-active'
+})
+
+export default router
